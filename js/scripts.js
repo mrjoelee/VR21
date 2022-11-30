b35ad164a3a87c4abfc790dad6e017efd11cc2dc
mrjoelee <joe.05.lee@outlook.com>
2022-11-30 17:32:21 -0500

    js practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,10 @@
-console.log("1. 30 console log statements");
+const sentence = "I am new to learning JavaScript";
 
-let constant = "needs to have a value no matter what";
-console.log(`2. const: ${constant}`);
+//using slice method --> "I am new"
 
-var message = "will save the data inside the window property"; 
-console.log(`3. var: ${window.message}`);
+console.log(sentence.slice(0,8));
+console.log(sentence.slice(-31,-23));
 
-const location1 = "top of mount everest";
-console.log(`4. location: ${location1}`);
+const word = sentence.split(" ");
+console.log(word[2]); // new
 
-console.log("5.", typeof location1);
-
-let IsJavaScriptDynamic = true;
-console.log(`6. Is JS a Dynamic Language: ${IsJavaScriptDynamic}`);
-
-console.log('7.', 12 > 13);
-
-let money = "time";
-let time = "money";
-console.log("8. is money equal to time:", money === time);
-
-let a = 5;
-console.log("9. a is equal to: %s", a); 
-console.log("10. a datatype is:", typeof a);
-
-let object;
-console.log("11. a undeclared variable using let is:", object)
-
-console.log(`12. the square root of negative number:`, Math.sqrt(-10));
-
-console.log("13. what's the typeof Math in JS:", typeof Math);
-
-let player = null;
-console.log('14. the value of player:', player);
-console.log(`15. typeof player:`, typeof player);
-
-console.log('16. typeof "true or false":', typeof true);
-
-const myName = "Joe";
-console.log('17. myName:', myName);
-console.log('18. the length of myName:', myName.length);
-console.log('19. the 1st index of myName:', myName[0]);
-
-for(var i = 0; i < 11; i ++){
-    console.log(`increment by 1+: ` + (i + 20));
-}
-
-console.log("here are the 30 console logs");
-
-let upperCase = myName.toUpperCase();
-let lowerCase = myName.toLowerCase();
-
-console.log(lowerCase);
-console.log(upperCase);
-
-console.log(upperCase === "JOE");
-
-let y = 10; 
-let z = "10";
-
-// == checks for value
-console.log(y == z);
-
-//checks for data type and value if one of them is false, then false. 
-console.log(y === z); 
-
-console.log( 0 == null);
-console.log(0 === null);

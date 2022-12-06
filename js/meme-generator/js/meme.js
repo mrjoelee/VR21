abf81697213cbef20490f786f63766e268bd2648
mrjoelee <joe.05.lee@outlook.com>
2022-12-05 22:05:12 -0500

    updated meme generator

--- js/meme-generator/js/meme.js
+++ js/meme-generator/js/meme.js
@@ -1,7 +1,13 @@
+//local storage
+const CARD_DATA_KEY = "card-data";
+const CARD_TITLE_ATTRIBUTE = "data-attribute";
+
 //getting the form for the addMemeModal
  const addForm = document.querySelector("#addMemeModal form"); 
 
  addForm.addEventListener("submit", handleAddSubmit);
+ //global object in javascript
+ window.addEventListener("load",addAllCardsToUI)
 
  function handleAddSubmit(event){
     //stopping from refreshing the page when "Submit"
@@ -17,6 +23,8 @@
     const dataCard = {imageUrl, topText, bottomText};
     createCard(dataCard);
 
+    //local save
+    addCarlToDB(dataCard);
 
     //resetting the form for the modal for new meme 
     addForm.reset();
@@ -42,14 +50,71 @@
     </div>`;
 
     //Add data to the url, top text, and bottom text
-    console.log(dataCard)
     cardCol.querySelector(".card-img-top").setAttribute("src",dataCard.imageUrl);
     cardCol.querySelector(".card-img-top").setAttribute("alt", dataCard.topText);
 
     cardCol.querySelector(".card-title").textContent = dataCard.topText;
     cardCol.querySelector(".card-text").textContent = dataCard.bottomText;
 
+    //delete method
+    //cardCol. will use the button within the innerHTML
+    const deleteBtn = cardCol.querySelector(".btn-danger");
+    deleteBtn.addEventListener("click", deleteCard);
+    
+    //class has to start with "data-"
+    //will use this attribute to know which card to delete from the DB 
+    cardCol.setAttribute(CARD_TITLE_ATTRIBUTE, dataCard.topText);
+
     //add cardCol to the page
     const cardContainer = document.querySelector("#cardContainer");
     cardContainer.append(cardCol);
  }
+
+ //adding the local storage (inspect->application->local Storage)
+ function addCarlToDB(dataCard){
+  let data = loadDataFromDB();
+   data.push(dataCard);
+
+   //save it back to local storage
+   saveDataToDB(data);
+ }
+
+ //adding to the page to display
+ function addAllCardsToUI(event){
+    let data = loadDataFromDB();
+    //loops every card that has been saved in the local db to display it 
+    data.forEach((dataCard)=> createCard(dataCard));
+ }
+
+ //loading from the database
+ function loadDataFromDB(){
+    let data = JSON.parse(localStorage.getItem(CARD_DATA_KEY));
+   if(!data){
+     data = [];
+   }
+   return data;
+ }
+
+//deleting card from page
+function deleteCard(event){
+    //can use same variable since it's different function
+    //select the card column that contains it.
+    // it's like querySelector, but works backwards - 
+    const deleteBtn = event.target;
+    const cardCol = deleteBtn.closest(".col");
+
+    //gets the attribute that was set on the top to find out the unique card
+    const topTextToDelete = cardCol.getAttribute(CARD_TITLE_ATTRIBUTE);
+    let data = loadDataFromDB();
+
+    //keep the ones that don't have this specific topText, filter creates a new array
+    data = data.filter((dataCard) => dataCard.topText !== topTextToDelete);
+    saveDataToDB(data);
+    cardCol.remove();
+
+}
+
+//save it back to local storage
+function saveDataToDB(data){
+   localStorage.setItem(CARD_DATA_KEY, JSON.stringify(data));
+}
